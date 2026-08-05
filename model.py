deee655ee25e3e8e1bb807d4067edb0e5224151c
aravindanjayapal-netizen <aravindanjayapal@gmail.com>
2026-08-06 00:32:04 +0530

    Update model.py with experimental changes

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
-# Generated on 2026-08-03T07:47:03.209280
+# Generated on 2026-08-05T19:02:03.646016
 
-def calculate_loss(y_true, y_pred):
-    import numpy as np
-    return np.mean((y_true - y_pred)**2)
+class SimpleModel:
+    def __init__(self):
+        self.weights = []
+    def forward(self, x):
+        return x
